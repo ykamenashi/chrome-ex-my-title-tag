7e4d3db879e9280278d8cbad4a40460dab0ac343
ykamenashi <kamenashi@cloud-ace.jp>
2024-08-12 17:05:42 +0900

    1st version.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chrome-ex-my-title-tag
 minor copy of 'show title tag' chrome addon.
+
+元ネタ: https://chromewebstore.google.com/detail/show-title-tag/pkbffbhglicfngmppdlpmpblfgnkdgio?hl=JA
+
+Chromeアドオン Show Title Tag がManifest V3対応で使えなくなったので、自分用に作った代替品です。
+
+![](img/show-title-tag-copy.png)
+
+# 使い方
+
+リポジトリをローカルにクローン後、Chromeの拡張機能管理画面から「パッケージ化されていない拡張機能を読み込む」を押して、フルパスを指定してください。
---
+++ script.js
@@ -0,0 +1,35 @@
+let msg = document.title;
+
+let indicator = document.createElement('div');
+indicator.style.position = 'fixed';
+indicator.style.bottom = 0;
+indicator.style.right = 0;
+indicator.style.zIndex = 999;
+
+indicator.style.backgroundColor = 'white';
+indicator.style.border = '1px solid #000';
+indicator.style.height = '25px';
+indicator.style.width = '100px';
+indicator.style.paddingLeft = '4px';
+
+indicator.addEventListener('mouseover', (ev)=>{
+  let elm = ev.target;
+  elm.style.opacity = 0;
+});
+indicator.addEventListener('mouseout', (ev)=>{
+  let elm = ev.target;
+  
+  elm.style.opacity = 100;
+});
+document.body.appendChild(indicator);
+
+let tmpElm = document.createElement('span');
+tmpElm.style.visibility = 'hidden';
+tmpElm.style.whiteSpace = 'pre';
+tmpElm.textContent = msg;
+document.body.appendChild(tmpElm);
+
+const width = tmpElm.offsetWidth;
+document.body.removeChild(tmpElm);
+indicator.style.width = width + 'px';
+indicator.textContent = msg;
